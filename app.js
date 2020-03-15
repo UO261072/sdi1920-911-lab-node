d74ccc1c9234258d942131d3ed7d3b8271ee030b
Aique Montes Maestre <UO261072@uniovi.es>
2020-03-15 15:32:34 +0100

    “SDI-NodeJS- 8,9,10 Recursos estáticos, Peticiones POST y parámetros, Enrutamiento y comodines”.

--- app.js
+++ app.js
@@ -2,18 +2,19 @@
 let express=require('express');
 let app=express();
 
+let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
 // Variables
 app.set('port', 8081);
 
+
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig);
+require("./routes/rcanciones.js")(app, swig);
 
 app.listen(app.get('port'),function(){
     console.log("Servidor activo");
