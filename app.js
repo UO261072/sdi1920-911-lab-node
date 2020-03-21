e928ff9fd1634ccd8e8cdb3891f84a161315a136
Aique Montes Maestre <UO261072@uniovi.es>
2020-03-21 12:26:00 +0100

    “SDI-NodeJS-8.3 - Arquitectura para el acceso a datos”

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
 // Variables
 app.set('port', 8081);
 app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00-xxrsn.mongodb.net:27017,tiendamusica-shard-00-01-xxrsn.mongodb.net:27017,tiendamusica-shard-00-02-xxrsn.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig);
-require("./routes/rcanciones.js")(app, swig,mongo);
+require("./routes/rusuarios.js")(app, swig,gestorBD);
+require("./routes/rcanciones.js")(app, swig,gestorBD);
 require("./routes/rautores.js")(app, swig);
 
 app.listen(app.get('port'),function(){
