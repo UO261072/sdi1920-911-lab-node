0bccf841f1dca8e2565112c0a20271cff518600d
Aique Montes Maestre <UO261072@uniovi.es>
2020-03-29 20:20:02 +0200

    SDI-NodeJS-9.2- Gestión de colecciones- eliminar documento

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -154,7 +154,17 @@ module.exports = function(app,swig,gestorBD) {
                 res.send(respuesta);
             }
         });
-    })
+    });
+    app.get('/cancion/eliminar/:id', function (req, res) {
+        let criterio = {"_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        gestorBD.eliminarCancion(criterio,function(canciones){
+            if ( canciones == null ){
+                res.send(respuesta);
+            } else {
+                res.redirect("/publicaciones");
+            }
+        });
+    });
 
     app.post('/cancion/modificar/:id', function (req, res) {
         let id = req.params.id;
